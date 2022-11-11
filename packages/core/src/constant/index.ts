98e21aa3e718fb70eff3cbe32c0e75b74f250a8b
CondorHero <love2xinwei@gmail.com>
2022-11-11 16:52:45 +0800

    fix: WPM 50 to 500

--- packages/core/src/constant/index.ts
+++ packages/core/src/constant/index.ts
@@ -1,5 +1,5 @@
 export const CONFIG_FILE_NAME = "frontmatter.json";
-export const WORDS_PER_MINUTE = 50;
+export const WORDS_PER_MINUTE = 500;
 export const TEMPLATE_FRONT_MATTER = {
 	data: {
 		date: new Date(new Date().toISOString()),
